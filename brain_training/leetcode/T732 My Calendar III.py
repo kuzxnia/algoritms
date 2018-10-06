bb468fee1401bc54087297262ff280d9ea822043
kuzxnia <kacper.kuzniarski@gmail.com>
2018-10-06 19:21:36 +0200

    LeetCode: added solution to task 732

---
+++ brain_training/leetcode/T732 My Calendar III.py
@@ -0,0 +1,19 @@
+
+class MyCalendarThree:
+
+    def __init__(self):
+        import bisect
+
+        self.events = []
+
+    def book(self, start, end):
+
+        bisect.insort_left(self.events, (start, 1))
+        bisect.insort(self.events, (end, 0))
+
+        res = 0
+        booking = 0
+        for _, state in self.events:
+            booking += 1 if state == 1 else -1
+            res = max(res, booking)
+        return res
